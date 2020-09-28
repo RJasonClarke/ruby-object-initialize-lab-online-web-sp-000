19f2cec30f389835e8e6159d244a141e2ec9fba7
Robert Clarke <r.jason.clarke@gmail.com>
2020-09-28 23:35:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,23 +3,3 @@ class Dog
     @name = "Fido"
   end
 end
-
-
-fido = Dog.new("Fido")
-
-
-class Dog
-  def initialize(breed)
-    @breed = breed
-  end
-
-  def breed= mutt
-    @breed = "Mutt"
-end
-
-def breed
-  @breed
-end
-end
-
-mutt = Dog.new("Mutt")
